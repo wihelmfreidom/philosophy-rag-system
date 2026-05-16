833f8fcf9c2e4e308452c99a9a2ef4168bb9534a
wihelmfreidom <zhangjiabao202403@163.com>
2026-05-16 15:39:18 +0800

    Update agent.py

--- src/agent.py
+++ src/agent.py
@@ -4,7 +4,7 @@ from langchain.agents import create_agent
 from langchain.messages import SystemMessage, HumanMessage
 
 myllm = ChatOpenAI(
-    api_key="c8ff5d6d099742b0855805b4e59245c4.1ORR5pA9RqwR5wcq",
+    api_key="",
     base_url="https://open.bigmodel.cn/api/paas/v4",  # 智谱 OpenAI 兼容端点
     model="glm-4-flash",
     temperature=0.7,  # 控制生成文本的随机程度，值越高越随机
