767e2e94a232b2fe9aa014f008157d99d6bf4835
wihelmfreidom <zhangjiabao202403@163.com>
2026-05-16 15:38:45 +0800

    Update Retrieval.py

--- src/Retrieval.py
+++ src/Retrieval.py
@@ -57,7 +57,7 @@ os.environ["OPENAI_BASE_URL"] = "https://api.deepseek.com"
 # 关键：streaming=True 启用流式
 # llm = ChatOpenAI(model="deepseek-chat", temperature=0.3, streaming=True)  # 必须开启！
 llm = ChatOpenAI(
-    api_key="c8ff5d6d099742b0855805b4e59245c4.1ORR5pA9RqwR5wcq",
+    api_key="",
     base_url="https://open.bigmodel.cn/api/paas/v4",  # 智谱 OpenAI 兼容端点
     model="glm-4-flash",
     temperature=0.7,
